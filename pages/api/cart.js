c1abdf248aaa400f8f9e46dea0a805f9704bffd8
kiittsunne <88187336+kiittsunne@users.noreply.github.com>
2022-01-02 20:44:28 +0800

    persist cart contents through local storage

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -1,35 +1,45 @@
-import { useReducer, useContext, createContext } from "react";
+import { useReducer, useContext, createContext, useEffect } from "react";
 
 const CartStateContext = createContext()
 const CartDispatchContext = createContext()
 
-const reducer = (state, action) => {
-    switch(action.type) {
-        case "ADD": 
-            const items = [...state, action.tea]
-            localStorage.setItem('cart', JSON.stringify(items))
-            return [...state, action.tea];
-        case "REMOVE":
-            const newArr = [...state];
-            newArr.splice(action.index, 1);
-            return newArr;
-        default: 
-            throw new Error(`unknown action ${action.type}`)
-    }
-}
-
 // CartProvider will wrap whole app to provide access to state/ dispatch
 export const CartProvider = ({ children }) => {
-    // let items = []
-    // // console.log(window)
-    // if (typeof window !== 'undefined') {
-    //     items = JSON.parse(localStorage.getItem('laughs'))
-    // }
 
-    const [state, dispatch] = useReducer(reducer, [])
+    const reducer = (cart, action) => {
+        switch (action.type) {
+            case "ADD":
+                return [...cart, action.tea]
+            case "REMOVE":
+                const removeArr = [...cart]
+                removeArr.splice(action.index, 1);
+                return removeArr;
+            // case "CLEAR_CART":
+            //     const clearArr = [...cart]
+            //     clearArr.splice(0, action.index.length)
+            //     return clearArr
+            default:
+                throw new Error(`unknown action ${action.type}`)
+        }
+    }
+
+    
+    const [cart, dispatch] = useReducer(reducer, [], () => {
+        if (typeof window !== 'undefined') {
+            return JSON.parse(localStorage.getItem('cart'))
+        } else {
+            return []
+        }
+    })
+
+    // source: https://www.youtube.com/watch?v=SOnMln3W0U8 
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
+
     return(
         <CartDispatchContext.Provider value={dispatch}>
-            <CartStateContext.Provider value={state}>
+            <CartStateContext.Provider value={cart}>
                 {children}
             </CartStateContext.Provider>
         </CartDispatchContext.Provider>
--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,6 +1,44 @@
 import Navbar from "../components/Navbar"
+import { useCart, useDispatchCart } from "./api/cart"
+
+
+export const CartItem = ({product, index, handleRemove}) => {
+    console.log(typeof(product.price))
+    return(
+        <div className="m-8 space-y-4">
+            <p>{product.name}</p>
+            <button onClick={() => handleRemove(index)}>Remove from cart</button>
+        </div>
+    )
+}
+
 
 export default function Cart () {
+    const teas = useCart()
+    const dispatch = useDispatchCart()
+    const totalPrice = teas.reduce((total, b) => total + b.price, 0)
+    
+    const handleRemove = (index) => {
+        dispatch({ type: "REMOVE", index });
+    }
+
+    // const handleClearCart = (index) => {
+    //     dispatch({ type: "CLEAR_CART", index})
+    // }
+
+    if (teas.length === 0) {
+        return (
+            <div className="m-12">
+                <div className="flex justify-center">
+                    <h1 className="font-serif font-semibold text-4xl text-slate-800">
+                        Cart
+                    </h1>
+                </div>
+                <Navbar />
+                <p className="mt-8">Cart is empty!</p>
+            </div>
+        );
+    }
     return(
         <div className="m-12">
             <div className="flex justify-center">
@@ -9,6 +47,22 @@ export default function Cart () {
                 </h1>
             </div>
             <Navbar />
+            {teas.map((tea, index) => (
+                <CartItem 
+                    handleRemove={handleRemove}
+                    key={index}
+                    product={tea}
+                    index={index}
+                />
+            ))}
+            <p>
+                Total price:{" "}
+                {totalPrice.toLocaleString("en", {
+                    style: "currency",
+                    currency: "USD"
+                })}
+            </p>
+            {/* <button onClick={handleClearCart}>Clear Cart</button> */}
         </div>
     )
 }
